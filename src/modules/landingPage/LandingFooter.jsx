9a845b7955ede5732577201b21b6d2bded2b49c2
Agles <20223tn005@utez.edu.mx>
2024-11-20 12:25:30 -0600

    [FIX] Arregle los colores

--- src/modules/landingPage/LandingFooter.jsx
+++ src/modules/landingPage/LandingFooter.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 
 const LandingFooter = () => {
     return (
-        <div className="grid border-t-1 border-primary py-5 bg-gray-100">
+        <div className="grid border-top-2 border-200 py-5 ">
 
            
             <div className="col-12 md:col-4 text-center">
                 <Card className="shadow-none border-none bg-transparent">
                     <p className="text-primary text-xl font-semibold mb-3">Redes Sociales</p>
-                    <div className="flex justify-content-center">
+                    <div className="flex justify-content-center text-primary">
                         <i className="pi pi-youtube mx-2" style={{ fontSize: '2.5rem' }}></i>
                         <i className="pi pi-facebook mx-2" style={{ fontSize: '2.5rem' }}></i>
                         <i className="pi pi-instagram mx-2" style={{ fontSize: '2.5rem' }}></i>
@@ -22,7 +22,7 @@ const LandingFooter = () => {
             <div className="col-12 md:col-4 text-left">
                 <Card className="shadow-none border-none bg-transparent">
                     <p className="text-primary text-xl font-semibold mb-3">Establecimiento</p>
-                    <ul className="list-none p-0 m-0">
+                    <ul className="list-none p-0 m-0 text-primary">
                         <li className="mb-2">Fotos</li>
                         <li className="mb-2">Membresías</li>
                         <li className="mb-2">Ofertas</li>
@@ -34,7 +34,7 @@ const LandingFooter = () => {
 
             
             <div className="col-12 md:col-4 text-left">
-                <Card className="shadow-none border-none bg-transparent">
+                <Card className="shadow-none border-none bg-transparent text-primary">
                     <p className="text-primary text-xl font-semibold mb-3">Contacto</p>
                     <p>Teléfono: +1 (555) 123-4567</p>
                     <p>Email: contacto@ejemplo.com</p>
