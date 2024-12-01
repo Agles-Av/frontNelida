0dea887c028974eb3d0cf6f5783ddce625f4f7df
Alexx573 <20213tn064@utez.edu.mx>
2024-12-01 16:30:23 -0600

    Ultimo commit del año

--- src/modules/empleado/Componentes/TablaUsuarios.jsx
+++ src/modules/empleado/Componentes/TablaUsuarios.jsx
@@ -16,51 +16,34 @@ import EditUsuarioEmp from './EditUsuarioEmp';
 import { Dropdown } from 'primereact/dropdown';
 
 const TablaUsuarios = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([]); // Datos originales de la API
   const [filteredData, setFilteredData] = useState([]); 
   const [emailSuggestions, setEmailSuggestions] = useState([]); 
+  const [showDialog, setShowDialog] = useState(false); // Controla la ventana emergente
+  const [roles, setRoles] = useState([]); 
   const [searchValue, setSearchValue] = useState(''); 
-  const [roles, setRoles] = useState([]);
-  const [showDialog, setShowDialog] = useState(false);
-  const [showEditDialog, setShowEditDialog] = useState(false);
-  const [selectedUser, setSelectedUser] = useState({});
   const [userType, setUserType] = useState('');
-  const messages = useRef(null);
 
   const userTypeOptions = [
     { label: 'Todos', value: '' },
     { label: 'Clientes', value: 'CLIENTE' },
     { label: 'Empleados', value: 'EMPLEADO' },
   ];
-
-  const getRoles = async () => {
-    try {
-      const response = await AxiosClient({ url: "/role/", method: "GET" });
-      if (!response.error) setRoles(response.data);
-    } catch (error) {
-      console.error('Error al obtener los roles:', error);
-    }
-  };
-
-  const getDatos = async () => {
-    try {
-      const response = await AxiosClient({ url: "/usuario/", method: "GET" });
-      if (!response.error) {
-        const usuariosFiltrados = response.data.filter(
-          (usuario) => usuario.role && usuario.role.nombre !== 'ADMIN'
-        );
-        setData(usuariosFiltrados);
-        setFilteredData(usuariosFiltrados); 
-      }
-    } catch (error) {
-      console.error('Error al obtener datos:', error);
-    }
-  };
-
-  useEffect(() => {
-    getDatos();
-    getRoles();
-  }, []);
+  const messages = useRef(null); // Usar useRef para la referencia de Messages
+  const [selectedUser, setSelectedUser] = useState([{
+    id: 0,
+    nombre: '',
+    apPaterno: '',
+    apMaterno: '',
+    email: '',
+    telefono: '',
+    contrasena: '',
+    edad: '',
+    role: '',
+    foto: null,
+  }]);
+  const [showEditDialog, setShowEditDialog] = useState(false);
+  
 
   // Lógica para filtrar por tipo y búsqueda
   useEffect(() => {
@@ -85,11 +68,86 @@ const TablaUsuarios = () => {
     setSearchValue(value);
   };
 
+  const getRoles = async () => {
+    try {
+      const response = await AxiosClient({
+        url: "/role/",
+        method: "GET",
+      });
+
+      if (!response.error) {
+        setRoles(response.data); // Establecer los roles
+      }
+    } catch (error) {
+      console.error('Error al obtener los roles:', error);
+    }
+  }
+
+  const getDatos = async () => {
+    try {
+      const response = await AxiosClient({
+        url: "/usuario/",
+        method: "GET",
+      });
+
+      if (!response.error) {
+        const usuariosFiltrados = response.data.filter(
+          (usuario) => usuario.role && usuario.role.nombre !== 'ADMIN'
+        );
+        setData(usuariosFiltrados);
+        setFilteredData(usuariosFiltrados); // Guarda solo los usuarios no admin
+      }
+    } catch (error) {
+      console.error('Error al obtener datos:', error);
+    } finally {
+    }
+  };
+
+  const changeStatus = async (rowData) => {
+    console.log(rowData);
+
+    try {
+      const response = await AxiosClient({
+        url: "/usuario/status/" + rowData.id,
+        method: "PATCH",
+      });
+
+      if (!response.error) {
+        getDatos();
+        messages.current.show({
+          sticky: true,
+          severity: 'success',
+          summary: 'Éxito',
+          detail: `El estado de ${rowData.nombre} se ha cambiado correctamente.`,
+          closable: true,
+        });
+      }
+    } catch (error) {
+      console.error('Error al obtener datos:', error);
+      // Agregar un mensaje de error si la solicitud falla
+      messages.current.show({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'No se pudo cambiar el estado del usuario.',
+      });
+    }
+  };
+
+  useEffect(() => {
+    getDatos();
+    getRoles();
+  }, []);
+
   const avatarBodyTemplate = (rowData) => (
-    <Avatar image={rowData.foto} shape="circle" style={{ marginRight: '0.5rem' }} />
+    <Avatar
+      image={rowData.foto}
+      shape="circle"
+      style={{ marginRight: '0.5rem' }}
+    />
   );
 
   const actionsBodyTemplate = (rowData) => {
+    // Función para abrir el popup de confirmación antes de cambiar el estado
     const handleStatusChange = () => {
       confirmPopup({
         target: document.activeElement,
@@ -99,24 +157,53 @@ const TablaUsuarios = () => {
         acceptLabel: 'Sí',
         rejectLabel: 'No',
         accept: () => changeStatus(rowData),
-        reject: () => {}
+        reject: () => { }
       });
     };
-
+  
     return (
       <>
+        {/* Botón de editar */}
         <Button
           icon="pi pi-pencil"
           className="p-button-rounded p-button-text"
           onClick={() => goEdit(rowData)}
+          id={`edit-btn-${rowData.id}`} // Identificador único
+          tooltip="Editar" tooltipOptions={{ showDelay: 1000, hideDelay: 50, position: 'top' }}
         />
-        <Button
-          icon={rowData.status ? "pi pi-ban" : "pi pi-check"}
-          className={`p-button-rounded ${rowData.status ? "p-button-danger" : "p-button-success"}`}
-          onClick={handleStatusChange}
-        />
+  
+        {/* Botón de activar/desactivar */}
+        {rowData.status ? (
+          <Button
+            icon="pi pi-ban" // Icono de desactivar
+            className="p-button-rounded p-button-danger"
+            onClick={handleStatusChange} // Mostrar el popup de confirmación
+            id={`status-btn-${rowData.id}`} // Identificador único
+            tooltip="Desactivar" tooltipOptions={{ showDelay: 1000, hideDelay: 50, position: 'top' }}
+          />
+        ) : (
+          /* Si el usuario está inactivo (status === false) */
+          <Button
+            icon="pi pi-check" // Icono de activar
+            className="p-button-rounded p-button-success"
+            onClick={handleStatusChange} // Mostrar el popup de confirmación
+            id={`status-btn-${rowData.id}`} // Identificador único
+            tooltip="Reactivar" tooltipOptions={{ showDelay: 1000, hideDelay: 50, position: 'top' }}
+          />
+        )}
       </>
     );
+  };
+  
+
+  // Muestra solo la última suscripción del arreglo
+  const suscripcionBodyTemplate = (rowData) => {
+    const suscripciones = rowData.suscripciones || []; // Asegúrate de que sea un arreglo
+    if (suscripciones.length > 0) {
+      const ultimaSuscripcion = suscripciones[suscripciones.length - 1]; // Toma la última suscripción
+      return ultimaSuscripcion.membresia.nombre; // Devuelve el nombre de la membresía
+    }
+    return 'Sin suscripción'; // Si no hay suscripciones
   };
 
   const goEdit = (rowData) => {
@@ -130,6 +217,14 @@ const TablaUsuarios = () => {
 
       <Messages ref={messages} />
 
+      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <Button
+          label="Añadir Usuario"
+          icon="pi pi-plus"
+          className="mb-3"
+          onClick={() => setShowDialog(true)}
+        />
+      </div>
       <div style={{ marginBottom: '1rem', display: 'flex', gap: '1rem' }}>
         <Dropdown
           value={userType}
@@ -169,7 +264,11 @@ const TablaUsuarios = () => {
         <Column header="Correo" field="email" />
         <Column header="Teléfono" field="telefono" />
         <Column header="Rol" field="role.nombre" />
-        <Column body={actionsBodyTemplate} style={{ textAlign: 'center' }} />
+        <Column
+          header="Suscripción Actual"
+          body={suscripcionBodyTemplate}
+        />
+        <Column body={actionsBodyTemplate} style={{ textAlign: '' }} header='Accciones'/>
       </DataTable>
 
       <CrearUsuarioEmp
@@ -178,7 +277,6 @@ const TablaUsuarios = () => {
         getUser={getDatos}
         messages={messages}
       />
-
       <EditUsuarioEmp
         abrir={showEditDialog}
         onHide={() => setShowEditDialog(false)}
